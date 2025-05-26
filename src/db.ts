a2ef3a4e2122874321d9defeed3d9aefa0ed54c1
Edwin Leon Herrera <edwin1224leon@gmail.com>
2025-05-25 19:57:37 -0600

    add certificate

--- src/db.ts
+++ src/db.ts
@@ -2,6 +2,7 @@ import { Pool } from 'pg'
 import dotenv from 'dotenv'
 
 dotenv.config()
+const cert = process.env.PG_SSL_CA?.replace(/\\n/g, '\n');
 
 export const pool = new Pool({
     user: process.env.PG_USER,
@@ -9,4 +10,19 @@ export const pool = new Pool({
     database: process.env.PG_DATABASE,
     password: process.env.PG_PASSWORD,
     port: Number(process.env.PG_PORT),
+    ssl:{
+        rejectUnauthorized: true,
+        ca: cert
+    },
 })
+
+pool.on('error', (err, client) => {
+    console.error('Unexpected error on idle client', err)
+    process.exit(-1)
+})
+
+pool.connect( (err : any, connection:any) => {
+    if (err) throw err;
+    console.log('Database is connected successfully !');
+    connection.release();
+});
